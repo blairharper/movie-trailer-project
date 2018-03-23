2a23307207d38c91ea00cd8eea4b8ee9c1f4a342
Blair <blair.harper@gmail.com>
2018-03-23 13:58:18 +0000

    Made media.py PRE8 compliant and added comments.

--- movie-trailer-project/media.py
+++ movie-trailer-project/media.py
@@ -1,10 +1,21 @@
 # Import dependencies
 import webbrowser
 
+
 # Movie class containing properties of movies
 class Movie():
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+    """Class to ceate a constructor for movies to be passed to fresh_tomatoes
+
+    Attributes:
+        title (str): title of movie
+        storyline (str): brief storyline of movie
+        poster_image: link to poster image to be displayed on movies page
+        trailer_youtube: link to youtube trailer to be displayed on click
+
+    """
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
